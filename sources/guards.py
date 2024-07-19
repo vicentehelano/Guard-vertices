242eb6ae931ba96612286fd9ba8aa42845381b60
Vicente Helano <vicentehelano@gmail.com>
2024-07-19 02:00:01 -0300

    Documented DelaunayTriangulation and added missing license disclaimers.

--- sources/guards.py
+++ sources/guards.py
@@ -1,1 +1,20 @@
 # -*- coding: utf-8 -*-
+"""\
+This is file `guards.py'.
+
+An implementation of a compact data structure for planar triangulations
+based on vertex-guards.
+
+Copyright (C) 2024 any individual authors listed elsewhere in this file.
+
+This code is marked with CC0 1.0 Universal. To view a copy of this license, visit
+https://creativecommons.org/publicdomain/zero/1.0/ or the accompanying
+LICENSE file.
+
+It is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+CC0 1.0 Universal for more details.
+
+Author(s): Vicente Sobrinho <vicente.sobrinho@ufca.edu.br>
+"""
